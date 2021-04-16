8f67890bccf136d1fdb22071f0e84a0d93fea599
isabellaortiz5 <isabellaortiz5@yahoo.com>
2021-04-16 09:34:15 -0500

    Data class created

---
+++ NetflixMovies/model/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixMovies
+{
+    class Data
+    {
+        private static List<Movies> movie = new List<Movies>();
+
+        public static void Main(String[] args)
+        {
+
+        }
+
+    }
+}
