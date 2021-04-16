da8a082e4c4a2c837df8d5ab606ac3b42e5857f2
isabellaortiz5 <isabellaortiz5@yahoo.com>
2021-04-16 09:48:59 -0500

    Reader updated in data

--- NetflixMovies/model/Data.cs
+++ NetflixMovies/model/Data.cs
@@ -11,7 +11,37 @@ namespace NetflixMovies
     {
         private static List<Movies> movie = new List<Movies>();
 
-        public static void Main(String[] args)
+        public void loadData()
+        {
+            using (var reader = new StreamReader(@"C:\Users\prestamo\Downloads\archive\netflix_titles.csv"))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    if (values[1] == "Movie")
+                    {
+                        string movieId = values[0];
+                        string title = values[2];
+                        string director = values[3];
+                        string cast = values[4];
+                        string countryOfOrigin = values[5];
+                        string datePublishedNet = values[6];
+                        int releaseYear = Int32.Parse(values[7]);
+                        var temp = values[9].Split(' ');
+                        int minutesOfMovie = Int32.Parse(temp[0]);
+                        string clasification = values[10];
+                        Movies mov = new Movies(movieId, title, director, cast, countryOfOrigin, datePublishedNet, releaseYear, minutesOfMovie, clasification);
+                        movie.Add(mov);
+
+                    }
+
+                }
+            }
+        }
+
+            public static void Main(String[] args)
         {
 
         }
