8cf4a0ec8e9706ca9dc5b24f81d126e92ce049a3
Windows 10 <Windows 10@DESKTOP-D148E36>
2021-04-15 23:14:22 -0500

    added Movie class

---
+++ NetflixMovies/model/Movie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixMovies
+{
+    class Movies
+    {
+
+        //Atributtes
+
+        private string movieId;
+        private string title;
+        private string director;
+        private string cast;
+        private string countryOfOrigin;
+        private string datePublishedNet;
+        private int releaseYear;
+        private int minutesOfMovie;
+        private string clasification;
+
+
+        public Movies(string movieId, string title, string director, string cast, string countryOfOrigin, string datePublishedNet, int releaseYear, int minutesOfMovie, string clasification)
+        {
+            this.MovieId = movieId;
+            this.Title = title;
+            this.Director = director;
+            this.Cast = cast;
+            this.CountryOfOrigin = countryOfOrigin;
+            this.DatePublishedNet = datePublishedNet;
+            this.ReleaseYear = releaseYear;
+            this.MinutesOfMovie = minutesOfMovie;
+            this.Clasification = clasification;
+
+        }
+
+        public string MovieId { get => movieId; set => movieId = value; }
+        public string Title { get => title; set => title = value; }
+        public string Director { get => director; set => director = value; }
+        public string Cast { get => cast; set => cast = value; }
+        public string CountryOfOrigin { get => countryOfOrigin; set => countryOfOrigin = value; }
+        public string DatePublishedNet { get => datePublishedNet; set => datePublishedNet = value; }
+        public int ReleaseYear { get => releaseYear; set => releaseYear = value; }
+        public int MinutesOfMovie { get => minutesOfMovie; set => minutesOfMovie = value; }
+        public string Clasification { get => clasification; set => clasification = value; }
+
+        public string toString()
+        {
+            return (MovieId + " ; " + Title + " ; " + Director + " ; " + Cast + " ; " + CountryOfOrigin + " ; " + DatePublishedNet + " ; " + ReleaseYear + " ; " + MinutesOfMovie + " ; " + Clasification + ". ");
+        }
+
+
+
+    }
+}
